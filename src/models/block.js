cad56cd3e1001a1ae5a17c399eb4d8554fc57ed7
Lamsaougar <yasser.lamsaougar@gmail.com>
2018-07-08 21:05:45 +0200

    quick fix

--- src/models/block.js
+++ src/models/block.js
@@ -7,10 +7,18 @@ export default class Block {
     this.fields = fields
     this.details = details
     this.suggestions = suggestions
+    setupBlock(this)
   }
 
   setPosition (positionX, positionY) {
     this.positionX = positionX
     this.positionY = positionY
   }
+}
+
+function setupBlock (block) {
+  Object.keys(block.fields).forEach(key => {
+    block.details[key] = block.details[key] || {}
+    block.suggestions[key] = block.suggestions[key] || []
+  })
 }
